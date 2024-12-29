eb82480b312797ea31c45a10dfef8cd55ea4278b
Kroos <dhiauddin@gmail.com>
2024-12-29 16:10:09 +0800

    --

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -6,13 +6,10 @@
 		</h2>
 	</x-slot>
 
-	<div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary" style="background-image: url({{ asset('images/errors/401-error.jpg')  }}); background-repeat: no-repeat; background-position: center center; height: 10%">
-	<div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
-		<div class="col-lg-6 px-0">
-			<h1 class="display-4 fst-italic">401 : Unauthorised Section</h1>
-			<p class="lead my-3">Please click on link below.</p>
-			<p class="lead mb-0"><a href="{{ url('/dashboard') }}" class="btn btn-lg btn-outline-secondary text-body-emphasis fw-bold">Home</a></p>
-		</div>
+	<div class="col-12-sm" >
+		<a href="{{ url('/dashboard') }}" class="">
+			<img src="{{ asset('images/errors/401-error.jpg') }}" class="img-fluid rounded " alt="">
+		</a>
 	</div>
 
 @section('js')
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,12 +6,10 @@
 		</h2>
 	</x-slot>
 
-	<div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary" style="background-image: url('{{ asset('images/errors/404-error.jpg') }}'); background-repeat: no-repeat; background-position: center center; ">
-		<div class="col-lg-6 px-0">
-			<h1 class="display-4 fst-italic">404 : Page Not Found</h1>
-			<p class="lead my-3">Please click on link below.</p>
-			<p class="lead mb-0"><a href="{{ url('/') }}" class="btn btn-lg btn-outline-secondary text-body-emphasis fw-bold">Home</a></p>
-		</div>
+	<div class="col-12-sm" >
+		<a href="{{ url('/dashboard') }}" class="">
+			<img src="{{ asset('images/errors/404-error.jpg') }}" class="img-fluid rounded " alt="">
+		</a>
 	</div>
 
 @section('js')
